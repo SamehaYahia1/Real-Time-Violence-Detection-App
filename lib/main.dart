b2bae89adea1ce1192ee34fe23c2ab2545c2055a
Sam9584 <sameha.ye3363@gmail.com>
2025-04-24 13:15:53 +0200

    new main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_application_1/Pages/choose_screen.dart';
 import 'package:flutter_application_1/Pages/login_screen.dart';
 import 'package:flutter_application_1/Pages/signup_screen.dart';
 import 'package:flutter_application_1/Pages/splash_screen.dart';
-import 'package:flutter_application_1/Pages/subscription_plan.dart';
 import 'package:flutter_application_1/UserOrAdminPage/user_or_admin.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_application_1/Pages/choose_your_plan.dart';
@@ -33,7 +32,11 @@ class MyApp extends StatelessWidget {
           '/choose': (context) => const ChooseScreen(),
           '/login': (context) => const LoginScreen(),
           '/signup': (context) => const SignupScreen(),
-          '/plans': (context) => const ChoosePlanScreen(userName: 'User'),
+          '/plans': (context) => const ChoosePlanScreen(
+                userName: 'User',
+                userId: '1',
+                SubscriptionId: '1',
+              ),
           '/userHome': (context) => const UserPage(userName: 'User'),
           '/adminHome': (context) => const UserPage(userName: 'Admin'),
         });
